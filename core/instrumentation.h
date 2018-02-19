11e676728787676b92dc4307c9e408c3c2314714
Ranjith Kumar <dsm@smackcoders.com>
2018-02-19 19:05:45 +0530

    The java doc and the instrumentation file committed

---
+++ core/instrumentation.h
@@ -0,0 +1,2 @@
+void setdata(char*,int,const char*,const char*,const char*,const char*,const char*,const char*,const char*,const char*);
+void senddata(char*,const char*,const char*,const char*,const char*,const char*,const char*);
